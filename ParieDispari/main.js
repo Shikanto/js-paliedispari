52078c4912cca99e8af4c29e34a9966ddada2a1b
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-18 17:15:32 +0200

    ex paridispari done

---
+++ ParieDispari/main.js
@@ -0,0 +1,104 @@
+
+/*  Creo funzione per generare un numero casuale per il pc da 1 a 5 */
+
+function randomNumber() {
+   
+    let randomNumberPc = Math.floor(Math.random()*5) + 1;
+    alert(`il numero del computer è ${randomNumberPc}`);
+    return randomNumberPc;
+
+} 
+
+/* Creo funzione per controllare se il numero è pari e dispari e far ritornare un valore per controllare se l'utente ha indovinato */
+
+function pariDispari(number) {
+
+/*  Definisco delle variabili per il check finale  */
+
+    let pari = "";
+    let dispari = "";
+
+/* Controllo se sia stato inserito un numero */
+
+    if (isNaN(number))
+    {
+      alert(`${number} non è un numero!`);
+      return;
+    }
+
+/* Controllo se il numero è pari */    
+    
+    let checkPari = number % 2;
+
+/* Se è pari ritorna la variabile pari per il check finale  */
+
+    if (checkPari === 0) {
+        alert(`${number}  è un numero Pari!`);
+        pari += "pari";
+        console.log(pari);
+        return pari;
+
+/* Se è dispari ritorna la variabile dispari per il check finale  */
+
+    } else {
+        alert(`${number}  è un numero Dispari!`);
+        dispari += "dispari"
+        console.log(dispari);
+        return dispari;
+    }
+}
+
+
+/* Scelgo tra pari e dispari */
+
+const makeYourChoice = prompt("Indovina se sarà un numero Pari o Dispari")
+
+/* Controllo se l'utente scrive correttamente pari o dispari */
+
+if (makeYourChoice != "pari" && makeYourChoice != "dispari"){
+    alert("hai sbagliato a scrivere, digita la parola 'pari' o 'dispari'")
+} else {
+    makeYourChoice;
+}
+
+/* Genero numeor casuale per l'utente */
+
+const randomNumberUtente = Math.floor(Math.random()*5) + 1;
+alert(`il numero dell'utente è ${randomNumberUtente}`);
+console.log(randomNumberUtente);
+
+/* Invoco funzione per generare numero casuale computer */
+
+let randomNumberPc = randomNumber();
+console.log(randomNumberPc);
+
+/* Funzione per sommare i due numeri casualmente creati */
+
+function sommaAndCheck(randomNumberUtente, randomNumberPc) {
+
+    const sommaNumbers = randomNumberUtente + randomNumberPc;
+
+    console.log(sommaNumbers);
+
+    return sommaNumbers;
+}
+
+/* invoco funzione per la somma */
+
+let sommaNumbers = sommaAndCheck(randomNumberUtente, randomNumberPc);
+
+/* invoco funzione per il final check */
+
+let finalResult = pariDispari(sommaNumbers);
+console.log(finalResult);
+
+/* Alert per l'utente per vedere se ha vinto o no */
+
+if (makeYourChoice === finalResult) {
+    alert("Complimenti! Hai Indovinato!")
+} else {
+    alert("Mi dispiace, ma ha vinto il computer")
+}
+
+
+
